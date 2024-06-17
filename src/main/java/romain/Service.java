36393acbeff7416a9fc1b40071412560192eaa3a
romainwgr <romain.wgr9@gmail.com>
2024-06-17 15:53:50 +0200

    corrigé

--- src/main/java/romain/Service.java
+++ src/main/java/romain/Service.java
@@ -18,34 +18,23 @@ public class Service implements Statistique {
     @Override
     public int prix() throws ArithmeticException {
         if (voitures.isEmpty()) {
-            throw new ArithmeticException();
+            throw new ArithmeticException("La liste des voitures est vide");
         }
 
-        for (int i = 0; i < voitures.size(); i++) {
-            if (i % 5 == 0 && i!=0) {
-                for (int j = i - 5; j < i; j++) {
-                    Voiture voiture_a_remise = voitures.get(j);
-                    int prix = voiture_a_remise.getPrix();
-                    double remise = prix * 0.05;
-                    if (remise > 20000) {
-                        remise = 20000;
-                    }
-                    int remise_int=(int)remise;
-                    voiture_a_remise.setPrix(prix - remise_int);
+        if (voitures.size() >= 5) {
+            for (int j = voitures.size() - 5; j < voitures.size(); j++) {
+                Voiture voiture = voitures.get(j);
+                int prix = voiture.getPrix();
+                double remise = prix * 0.05; // Calculer la remise
+                if (remise > 20000) {
+                    remise = 20000;
                 }
+                int remiseInt = (int) remise;
+                voiture.setPrix(prix - remiseInt);
             }
         }
+
         return voitures.get(voitures.size() - 1).getPrix();
     }
 
-    public static void main(String[] args) {
-        Service s1 = new Service();
-
-        s1.ajouter(new Voiture("Renault", 10000));
-        s1.ajouter(new Voiture("Peugeot", 15000));
-        s1.ajouter(new Voiture("Lamborghini", 20000));
-        s1.ajouter(new Voiture("BMW", 25000));
-        s1.ajouter(new Voiture("Mercedes", 5000000));
-        int dernierPrix = s1.prix();
-    }
 }
