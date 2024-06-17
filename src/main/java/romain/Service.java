4a02698af88cc0f0c96542e9e5ee1b4951f474a5
romainwgr <romain.wgr9@gmail.com>
2024-06-17 15:38:44 +0200

    Corrigé

--- src/main/java/romain/Service.java
+++ src/main/java/romain/Service.java
@@ -22,7 +22,7 @@ public class Service implements Statistique {
         }
 
         for (int i = 0; i < voitures.size(); i++) {
-            if (i % 5 == 0) {
+            if (i % 5 == 0 && i!=0) {
                 for (int j = i - 5; j < i; j++) {
                     Voiture voiture_a_remise = voitures.get(j);
                     int prix = voiture_a_remise.getPrix();
@@ -35,6 +35,17 @@ public class Service implements Statistique {
                 }
             }
         }
-        return voitures.get(-1).getPrix();
+        return voitures.get(voitures.size() - 1).getPrix();
+    }
+
+    public static void main(String[] args) {
+        Service s1 = new Service();
+
+        s1.ajouter(new Voiture("Renault", 10000));
+        s1.ajouter(new Voiture("Peugeot", 15000));
+        s1.ajouter(new Voiture("Lamborghini", 20000));
+        s1.ajouter(new Voiture("BMW", 25000));
+        s1.ajouter(new Voiture("Mercedes", 5000000));
+        int dernierPrix = s1.prix();
     }
 }
